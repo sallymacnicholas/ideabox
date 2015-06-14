8d8418e588bdab193d695b083d5b77ae86835f9a
Sally MacNicholas <smacnicholas@gmail.com>
2015-06-14 00:13:49 -0600

    create_new_user closed #1

---
+++ test/integration/create_user_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class CreateUserTest < ActionDispatch::IntegrationTest
+  include Capybara::DSL
+
+  test 'user can create account' do
+    visit new_user_path
+    fill_in 'Username', with: 'sally'
+    fill_in 'Password', with: 'password'
+    click_button 'Create Account'
+
+    assert page.has_content?('Welcome, sally')
+  end
+end
